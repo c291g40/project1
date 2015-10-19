6c922e8ab4010d81fdf8d0e9c7d3b2b1b1b97a70
eorodrig <eorodrig@ualberta.ca>
2015-10-19 15:42:21 -0600

    fixed some stuff for python 3

--- Project/main.py
+++ Project/main.py
@@ -1,17 +1,18 @@
 import sys;
-#import cx_Oracle;
-
+import cx_Oracle;
+import test;
 
 class SplashScreen:
 	email = "";
 	password = "";
-	loginTime = "";
+	logoutTime = "";
 	userVerified = False;
+	isAgent = False;
 
 	def start(self):
 		while(self.userVerified == False):
 			print("Welcome to group 40's airline ticket system");
-			userInput = raw_input("Please select an option: Login, Register, Exit:");
+			userInput = input("Please select an option: Login, Register, Exit:");
 			if(userInput.lower()=="exit"):
 				self.userVerified = True;
 				exit();
@@ -24,19 +25,25 @@ class SplashScreen:
 			else:
 				self.userVerified = False;
 				print("\ninvalid User Input. Please select a valid input.");
+		return self.userVerified;
+
 	
 
 	def userSignin(self):
-		self.email = raw_input("Please enter your email address: ");
-		self.password = raw_input("Please enter your password: ");
+		self.email = input("Please enter your email address: ");
+		self.password = input("Please enter your password: ");
+		
+		print("Login Successful, Type logout at anytime to logout of your account.");
 		return True;
 	
 	
 	
 
 	def registerUser(self):
-		self.email = raw_input("Please enter an email address: ");
-		self.password = raw_input("Please enter a password: ");
+		self.email = input("Please enter an email address: ");
+		self.password = input("Please enter a password: ");
+		
+		print("Account created. Type logout at anytime to logout of your account.");
 		return True;
 
 	def getEmail(self):
@@ -44,6 +51,12 @@ class SplashScreen:
 		
 	def getPassword(self):
 		return self.password;
+	
+	def logOut(self):
+		self.userVerified = False;
+		
+	def isAgent(self):
+		return self.isAgent;
 	
 #class SearchForFlight:
 
@@ -53,10 +66,21 @@ class SplashScreen:
 
 
 ##splash screen stuff
+test.testing();
 ss = SplashScreen();
-ss.start();
-email = ss.getEmail();
-password = ss.getPassword();
+authenticated = ss.start();
 
+while(authenticated == True):
+	email = ss.getEmail();
+	password = ss.getPassword();
+	isAgent = ss.isAgent();
+	
+	
+	if (isAgent):
+		userInput = input("Please Select one of the following options: \nSearch for Flights(1), \nMake a booking(2), \nList existing bookings(3), \nCancel a booking(4), \nRecord flight departure (5), \nRecord flight arrival (6), \nLogout (9)");
+	else:
+	        userInput = input("Please Select one of the following options: \nSearch for Flights(1), \nMake a booking(2), \nList existing bookings(3), \nCancel a booking(4), \nLogout (9)");
+	
+	print(userInput);
 
-print("email: " + email + " \npassword: " + password);
+	print("email: " + email + " \npassword: " + password);
