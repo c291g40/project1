8bc834462f2930170ab7ada168bc38994c7db88d
maabbasi <abbasi1@ualberta.ca>
2015-10-22 12:37:31 -0600

    added acode checking

--- Project/search.py
+++ Project/search.py
@@ -19,28 +19,62 @@ def getDepartureDate ():
             return depart_date
 
 
-def validAirport ():
+def validAirport (acode):
     try:
+        acode = acode.upper()
+        acodeList = []
+        matchList = []
+        validAirport = False
+
+        # connect to database
         connection = cx_Oracle.connect('abbasi1/c291database@gwynne.cs.ualberta.ca:1521/CRS')
         curs = connection.cursor()
+
         # get list of airports
         curs.execute("SELECT acode FROM airports")
         for row in curs:
-            print(row)
+            acodeList.append(row[0].upper())
+        
         # if airport exists return code
-        # else look for matches and get user to choose
+        if acode in acodeList:
+            validAirport = True
+
+        if not(validAirport):
+            # get list of matching airports' name, city
+            curs.execute("SELECT acode,name,city FROM airports")
+            for row in curs:
+                if acode in row[1].upper() or acode in row[2].upper():
+                    temp = [row[0].upper(), row[1].upper(), row[2].upper()]
+                    matchList.append(temp)
+        
+            # get user to select a matching airport
+            validInput = False
+            for match in matchList:
+                print(matchList[0].ljust(4),matchList[1].ljust(31),matchList[2].ljust(16))
+            while not(validInput):
+                print("/nThese airports matched your entry.")
+                airport = input("Please type an ACODE from the above list: ")
+                airport = airport.upper()
+                if airport in [acode for acode=matchList[0] in matchList]:
+                    validInput = True
+             
+        # close the connection
         curs.close
         connection.close()
+
+        # return the airport code
+        return(acode)
     # error catching sourced from cx_Oracle tutorial
     except cx_Oracle.DatabaseError as exc:
         error, = exc.args
         print( sys.stderr, "Oracle code:", error.code)
-        print( sys.stderr, "Oracle message:", error.message)    
+        print( sys.stderr, "Oracle message:", error.message)
 
 
 def main ():
-    #source = input("Enter the source airport: ")
+    source = input("Enter the source airport: ")
     #dest = input("Enter the destination airport: ")
     #depart_date = getDepartureDate()
-    validAirport()
+    source = validAirport(source)
+    #validAirport(dest)
 main()
