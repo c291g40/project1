52375388a49ca476bc17c74fd124b221d3985d58
maabbasi <abbasi1@ualberta.ca>
2015-10-26 20:38:58 -0600

    Fixed prompt for time.

--- Project/updateTime.py
+++ Project/updateTime.py
@@ -2,8 +2,12 @@ import datetime
 import sys
 import cx_Oracle
 
+# This function updates the actual arrival and departure time of flights.
 def updateTime (connectionString, changeType):
+    
     print("Updating %s Time." %(changeType))
+    
+    # Gets the flight number.
     validInput = False
     while not(validInput):
         flightNo = input("Enter the flight number: ")
@@ -12,6 +16,7 @@ def updateTime (connectionString, changeType):
         else:
             validInput = True
 
+    # Gets the departure date.
     validInput = False
     while not(validInput):
         depart_date = input("Enter the departure date as DD/MM/YEAR: ")
@@ -26,9 +31,10 @@ def updateTime (connectionString, changeType):
         else:
             validInput = True
     
+    # Gets the time to set as act_arr or act_dep.
     validInput = False
     while not(validInput):
-        newTime = input("Please enter the %s time of the flight as HH:MM: " %(changeType)).strip()
+        newTime = input("Please enter the %s time of the flight as DD/MM/YYYY HH:MM: " %(changeType)).strip()
         try:
             if int(newTime[11:13]) not in range(24):
                 print("Invalid entry for hours.")
@@ -38,15 +44,21 @@ def updateTime (connectionString, changeType):
             print("Invalid entry.")
         else:
             validInput = True
+
+    # Updates the database.
     try:
+        # Creating connection.
         connection = cx_Oracle.connect(connectionString)
         curs = connection.cursor()
+
+        # Selects correct arr/dep time and updates in database.
         if changeType == "Arrival":
             curs.execute("UPDATE sch_flights SET act_arr_time = TO_DATE('%s','DD/MM/YYYY HH24:MI') WHERE TO_CHAR(dep_date, 'DD/MM/YYYY') = '%s' AND flightno = '%s'" %(newTime,depart_date,flightNo))
         else:
             curs.execute("UPDATE sch_flights SET act_dep_time = TO_DATE('%s','DD/MM/YYYY HH24:MI') WHERE TO_CHAR(dep_date, 'DD/MM/YYYY') = '%s' AND flightno = '%s'" %(newTime,depart_date,flightNo))
         curs.execute("COMMIT")
-        # close the connection
+
+        # Close the connection.
         curs.close
         connection.close()
         
@@ -55,4 +67,4 @@ def updateTime (connectionString, changeType):
         print( sys.stderr, "Oracle code:", error.code)
         print( sys.stderr, "Oracle message:", error.message)
 
-updateTime('abbasi1/c291database@gwynne.cs.ualberta.ca:1521/CRS',"Departure")
+# updateTime('abbasi1/c291database@gwynne.cs.ualberta.ca:1521/CRS',"Departure")
